e7e15330168dd6b1d5f2fdc3e85d45b479bb302a
Riley1101 <arkardev00@gmail.com>
2023-09-06 09:11:11 -0400

    added longest_repeating_character_replacement

--- src/neet/longest_repeating_character_replacement.ts
+++ src/neet/longest_repeating_character_replacement.ts
@@ -8,7 +8,6 @@ function characterReplacement() {
   let res = 0;
   let map: Record<string, number> = {};
   for (let right = 0; right < s.length; right++) {
-
     map[s[right]] = map[s[right]] ? map[s[right]] + 1 : 1;
 
     let most = Object.keys(map).reduce((a, b) => (map[a] > map[b] ? a : b));
@@ -22,9 +21,20 @@ function characterReplacement() {
   return res;
 }
 
-
 function longest_character_replace() {
-
+  let left = 0;
+  let res = 0;
+  let map: Record<string, number> = {};
+  for (let right = 0; right < s.length; right++) {
+    map[s[right]] = map[s[right]] ? (map[s[right]] += 1) : 1;
+    let most = Object.keys(map).reduce((a, b) => (map[a] > map[b] ? a : b));
+    res = Math.max(res, right - left + 1);
+    while (right - left + 1 - map[most] > k) {
+      left += 1;
+      map[s[left]] += 1;
+    }
+  }
+  return res;
 }
 
-l(characterReplacement());
+l(longest_character_replace());
